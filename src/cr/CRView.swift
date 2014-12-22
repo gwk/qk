ad80461b29db69516d0b5feaef8bc1f9d364a83f
George King <george.w.king@gmail.com>
2014-12-22 15:15:20 -0500

    CRView.name is temporarily an error on osx.

--- src/cr/CRView.swift
+++ src/cr/CRView.swift
@@ -10,27 +10,6 @@
 #endif
 
 
-#if os(OSX)
-  // TODO: use NSAccessibilityIdentifierAttribute?
-  
-var _viewTagNames: [String] = [""]
-var _viewNameTags: [String:Int] = ["":0]
-
-func regViewNameTag(n: String) -> Int {
-  assert(_viewTagNames.count == _viewNameTags.count)
-  let ot = _viewNameTags[n]
-  if let t = ot {
-    assert(n == _viewTagNames[t])
-    return t
-  } else {
-    let c = _viewTagNames.count
-    _viewTagNames.append(n)
-    _viewNameTags[n] = c
-    return c
-  }
-}
-#endif
-
 extension CRView {
   
   convenience init(size: CGSize) { self.init(frame: CGRect(size)) }
@@ -43,7 +22,7 @@ extension CRView {
   var name: String {
     get {
       #if os(OSX)
-        return _viewTagNames[tag]
+        fatalError("not implemented")
         #else
         return accessibilityIdentifier
       #endif
@@ -51,7 +30,7 @@ extension CRView {
     set {
       assert(newValue.isSym)
       #if os(OSX)
-        tag = regViewNameTag(newValue)
+        fatalError("not implemented")
       #else
         accessibilityIdentifier = newValue
       #endif

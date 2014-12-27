dbeb7be1eccebfa18dfe518f40fa92db79db3e0e
George King <george.w.king@gmail.com>
2014-12-27 11:18:28 -0500

    NSNotificationCenter.

---
+++ src/foundation/NSNotificationCenter.swift
@@ -0,0 +1,9 @@
+// © 2014 George King.
+// Permission to use this file is granted in license-qk.txt.
+
+import Foundation
+
+
+extension NSNotificationCenter {}
+
+func noteCenter() -> NSNotificationCenter { return NSNotificationCenter.defaultCenter() }

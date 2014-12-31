09b4d5484fc3f78f98ad05a21506b88008e04d21
George King <george.w.king@gmail.com>
2014-12-30 22:21:13 -0500

    UIViewController methods.

--- src/ios/UIViewController.swift
+++ src/ios/UIViewController.swift
@@ -8,17 +8,28 @@ extension UIViewController {
   
   var nav: UINavigationController? { return navigationController }
   
+  class func fromStoryboard() -> UIViewController {
+    let s = NSStringFromClass(self)
+    let r = s.rangeOfString(".")!
+    let n = s.substringFromIndex(r.endIndex)
+    println("fromStoryboard: \(n)")
+    let board = UIStoryboard(name: n, bundle: nil)
+    return board.instantiateInitialViewController() as UIViewController
+  }
+  
   func present(c: UIViewController, animated: Bool = true, completion: Action? = nil) {
+    // TODO: should this be parentViewController instead of just nav?
     (nav as UIViewController?).or(self).presentViewController(c, animated: animated, completion: completion)
   }
   
-  func presentNavRoot(root: UIViewController, animated: Bool = true, completion: Action? = nil) {
+  func presentNavRoot(root: UIViewController, isBarHidden: Bool = false, animated: Bool = true, completion: Action? = nil) {
     root.navigationItem.leftBarButtonItem = UIBarButtonItem(
       title: root.closeButtonTitle,
       style: .Plain,
       target: root,
       action:"dismissSelfAction")
     let n = UINavigationController(rootViewController: root)
+    n.navigationBarHidden = isBarHidden
     present(n, animated: animated, completion: completion)
   }
   

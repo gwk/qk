5eb88e7a1ca0cf039665ba377ec76e3e4c90dce6
George King <george.w.king@gmail.com>
2015-01-05 20:32:52 -0500

    addBackItem.

---
+++ src/ios/UINavigationItem.swift
@@ -0,0 +1,12 @@
+// © 2014 George King.
+// Permission to use this file is granted in license-qk.txt.
+
+import UIKit
+
+
+extension UINavigationItem {
+  
+  func addBackItem(title: NSString = "Back") {
+    backBarButtonItem = UIBarButtonItem(title: title, style: .Plain, target: nil, action: nil)
+  }
+}

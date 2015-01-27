5016fafd437c1aa2f97f73f24462456a8d7e1667
George King <george.w.king@gmail.com>
2015-01-26 21:25:34 -0500

    UIView: fromNib, loadSubviewsFromNib.

--- src/ios/UIView.swift
+++ src/ios/UIView.swift
@@ -6,5 +6,21 @@ import UIKit
 
 extension UIView {
   
+  class func fromNib(name: String, owner: AnyObject) -> [AnyObject] {
+    return NSBundle.mainBundle().loadNibNamed(name, owner: owner, options: nil)
+  }
+
+  func loadSubviewsFromNib(var owner: NSObject? = nil, var name: String? = nil) {
+    if owner == nil {
+      owner = self
+    }
+    if name == nil {
+      name = owner!.dynamicTypeName
+    }
+    let subviews = UIView.fromNib(name!, owner: owner!)
+    for v in subviews {
+      addSubview(v as UIView)
+    }
+  }
 }
 

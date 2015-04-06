62d5c3bb1b5c68e7fda1d20c2f36beb9ba7128d2
George King <george.w.king@gmail.com>
2015-04-06 21:50:49 +0200

    math: better names for posix constants.

--- src/core/math.swift
+++ src/core/math.swift
@@ -1,2 +1,18 @@
-// © 2014 George King.
+// © 2015 George King.
 // Permission to use this file is granted in license-qk.txt.
+
+
+let k_e             = M_E         // e
+let k_log2_e        = M_LOG2E     // log2(e)
+let k_log10_e       = M_LOG10E    // log10(e)
+let k_ln_2          = M_LN2       // ln(2)
+let k_ln_10         = M_LN10      // ln(10)
+let k_pi            = M_PI        // pi
+let k_2_pi          = 2 * M_PI
+let k_pi_div_2      = M_PI_2      // pi/2
+let k_pi_div_4      = M_PI_4      // pi/4
+let k_1_div_pi      = M_1_PI      // 1/pi
+let k_2_div_pi      = M_2_PI      // 2/pi
+let k_2_div_sqrt_pi = M_2_SQRTPI  // 2/sqrt(pi)
+let k_sqrt_2        = M_SQRT2     // sqrt(2)
+let k_1_div_sqrt_2  = M_SQRT1_2   // 1/sqrt(2)

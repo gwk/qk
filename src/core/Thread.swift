4b57d0375e47b88d3cc27ed5a1754de9681188cb
George King <george.w.king@gmail.com>
2015-06-09 10:03:11 -0400

    Lock, Thread.

---
+++ src/core/Thread.swift
@@ -0,0 +1,30 @@
+// © 2015 George King.
+// Permission to use this file is granted in license-qk.txt.
+
+
+import Foundation
+
+
+let processorCount = sysconf(_SC_NPROCESSORS_CONF)
+
+
+class Thread: NSThread {
+  
+  let action: Action
+  
+  init(action: Action) {
+    self.action = action
+    super.init()
+  }
+  
+  override func main() {
+    action()
+  }
+}
+
+
+func spawnThread(action: Action) -> Thread {
+  let thread = Thread(action: action)
+  thread.start()
+  return thread
+}

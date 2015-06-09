f2b609da228329c0fbb3c22b5d834f76f66733d9
George King <george.w.king@gmail.com>
2015-06-09 10:02:48 -0400

    Arr; AreaBuffer inherits from Arr.

--- src/core/AreaBuffer.swift
+++ src/core/AreaBuffer.swift
@@ -4,51 +4,39 @@
 import Foundation
 
 
-class AreaBuffer<T> {
+class AreaBuffer<T>: Arr<T> {
   typealias Row = ArraySlice<T>
   private var _size: V2I
-  private var _array: ContiguousArray<T>
   
-  init() {
+  override init() {
     _size = V2I()
-    _array = ContiguousArray<T>()
+    super.init()
   }
   
   var size: V2I { return _size }
-  var count: Int { return _array.count }
   
+  override func resize(count: Int, val: T) { fatalError("use resize(size: V2I, val: T)") }
+
   func resize(size: V2I, val: T) {
-    _array.removeAll(keepCapacity: true)
+    super.resize(size.x * size.y, val: val)
     _size = size
-    let c = _size.x * _size.y
-    _array.extend(lazy(0..<c).map({ (i) in return val }))
   }
   
   func row(row: Int) -> Row {
     let off = size.x * row
-    return _array[off..<(off + size.x)]
+    return self[off..<(off + size.x)]
   }
   
   func el(i: Int, _ j: Int) -> T {
-    return _array[_size.x * j + i]
+    return self[_size.x * j + i]
   }
 
   func el(coord: V2I) -> T { return el(coord.x, coord.y) }
   
   func setEl(i: Int, _ j: Int, _ val: T) {
-    _array[_size.x * j + i] = val
+    self[_size.x * j + i] = val
   }
   
   func setEl(coord: V2I, _ val: T) { setEl(coord.x, coord.y, val) }
-
-  subscript (i: Int) -> T {
-    get { return _array[i] }
-    set { _array[i] = newValue }
-  }
-  
-  func withUnsafeBufferPointer<R>(@noescape body: (UnsafeBufferPointer<T>) -> R) -> R {
-    return _array.withUnsafeBufferPointer(body)
-  }
-
 }
 
---
+++ src/core/Arr.swift
@@ -0,0 +1,45 @@
+// © 2015 George King.
+// Permission to use this file is granted in license-qk.txt.
+
+import Foundation
+
+
+class Arr<T>: CollectionType {
+  private var _array: ContiguousArray<T>
+  
+  init() {
+    _array = ContiguousArray<T>()
+  }
+
+  var count: Int { return _array.count }
+
+  typealias Generator = ContiguousArray<T>.Generator
+  
+  func generate() -> Generator { return _array.generate() }
+  
+  typealias Index = ContiguousArray<T>.Index
+
+  var startIndex: Index { return _array.startIndex }
+
+  var endIndex: Index { return _array.endIndex }
+
+  subscript (i: Int) -> T {
+    get { return _array[i] }
+    set { _array[i] = newValue }
+  }
+  
+  subscript (range: Range<Index>) -> ArraySlice<T> {
+    get { return _array[range] }
+    set { _array[range] = newValue }
+  }
+  
+  func resize(count: Int, val: T) {
+    _array.removeAll(keepCapacity: true)
+    _array.extend(lazy(0..<count).map({ (i) in return val }))
+  }
+  
+  func withUnsafeBufferPointer<R>(@noescape body: (UnsafeBufferPointer<T>) -> R) -> R {
+    return _array.withUnsafeBufferPointer(body)
+  }
+}
+

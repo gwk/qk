d135be71bd33ffcc2ad6ef1f1f8dfa415e24df7a
George King <george.w.king@gmail.com>
2015-08-14 19:26:19 -0400

    check now split into user-facing and internal.

--- src/core/check.swift
+++ src/core/check.swift
@@ -1,9 +1,19 @@
-// © 2014 George King.
-// Permission to use this file is granted in license-qk.txt.
+// © 2014 George King. Permission to use this file is granted in license-qk.txt.
+
+import Darwin
 
 
-func check(condition: Bool, @autoclosure _ message: () -> String, file: StaticString = __FILE__, line: UInt = __LINE__) {
+func check(condition: Bool, @autoclosure _ message: () -> String) {
   if !condition {
-    fatalError(message, file: file, line: line)
+    std_err.write("error: ")
+    std_err.write(message())
+    std_err.write("\n")
+    exit(1)
+  }
+}
+
+func check(condition: Bool, file: StaticString = __FILE__, line: UInt = __LINE__) {
+  if !condition {
+    fatalError("check failure", file: file, line: line)
   }
 }

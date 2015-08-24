5abe168e0ae0c9c3eccd1b956b32e8eb292c7288
George King <george.w.king@gmail.com>
2015-08-24 12:30:19 -0700

    SequenceType: replace.

--- src/std/SequenceType.swift
+++ src/std/SequenceType.swift
@@ -3,7 +3,7 @@
 
 extension SequenceType {
   
-  func mapThrows<T>(@noescape transform: (Self.Generator.Element) throws -> T) throws -> [T] {
+  func mapThrows<T>(@noescape transform: (Generator.Element) throws -> T) throws -> [T] {
     var a = [T]()
     for e in self {
       let v = try transform(e)
@@ -14,4 +14,44 @@ extension SequenceType {
 }
 
 
+extension SequenceType where Generator.Element: Equatable {
 
+  func replace(query: Generator.Element, with: Generator.Element) -> [Generator.Element] {
+    var result: [Generator.Element] = []
+    for e in self {
+      if e == query {
+        result.append(with)
+      } else {
+        result.append(e)
+      }
+    }
+    return result
+  }
+
+  func replace<Q: CollectionType, W: CollectionType where Q.Generator.Element == Generator.Element, W.Generator.Element == Generator.Element>(query: Q, with: W) -> [Generator.Element] {
+    if query.isEmpty {
+      return Array(self)
+    }
+    var buffer: [Generator.Element] = []
+    var result: [Generator.Element] = []
+    var i = query.startIndex
+    for e in self {
+      if e == query[i] {
+        i = i.successor()
+        if i == query.endIndex {
+          result.extend(with)
+          buffer.removeAll()
+          i = query.startIndex
+        } else {
+          buffer.append(e)
+        }
+      } else {
+        result.extend(buffer)
+        result.append(e)
+        buffer.removeAll()
+        i = query.startIndex
+      }
+    }
+    return result
+  }
+}

08cb6a4cd88648c0fb9d81e28272e39f34a79edb
George King <george.w.king@gmail.com>
2015-10-14 18:06:48 -0400

    split out File+Dispatch dependency.

--- src/core/File.swift
+++ src/core/File.swift
@@ -1,7 +1,6 @@
 // Copyright © 2015 George King. Permission to use this file is granted in ploy/license.txt.
 
 import Darwin
-import Dispatch
 
 
 class File: CustomStringConvertible {
@@ -80,32 +79,6 @@ class File: CustomStringConvertible {
       return false
     }
     return true
-  }
-
-  func createDispatchSource(modes: DispatchFileModes, queue: DispatchQueue = dispatchMainQueue,
-    registerFn: Action? = nil, cancelFn: Action? = nil, eventFn: Action) -> DispatchSource {
-      let source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE, Uns(descriptor), modes.rawValue, queue)!
-      if let rf = registerFn {
-        dispatch_source_set_registration_handler(source, rf)
-      }
-      // the cancel handler retains the file to prevent a race condition
-      // where the file descriptor gets reused but the source is not yet canceled.
-      // see the documentation for dispatch_source_set_cancel_handler.
-      let _self = self
-      if let cf = cancelFn {
-        dispatch_source_set_cancel_handler(source) {
-          [_self] in
-          _self // no-op.
-          cf()
-        }
-      } else {
-        dispatch_source_set_cancel_handler(source) {
-          [_self] in
-          _self
-        }
-      }
-    dispatch_source_set_event_handler(source, eventFn)
-    return source
   }
 
   static func changePerms(path: String, _ perms: Perms) throws {

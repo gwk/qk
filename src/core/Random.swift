e122e94e9ca4203164bf7cf3ecc5604170d08882
George King <george.w.king@gmail.com>
2015-12-02 15:27:41 -0500

    rename Random methods.

--- src/core/Random.swift
+++ src/core/Random.swift
@@ -14,7 +14,7 @@ class Random {
     self.state1 = state1
   }
 
-  func next() -> U64 {
+  func raw() -> U64 {
     let s1 = state0
     let s0 = state1
     state0 = s0
@@ -26,35 +26,47 @@ class Random {
     return state1
   }
 
-  func next(max: U64) -> U64 {
+  func u64(end: U64) -> U64 {
     // unbiased random.
-    let bias_rem = ((U64.max % max) + 1) % max
-    let max_unbiased = U64.max - bias_rem
+    let bias_rem = ((U64.max % end) + 1) % end
+    let max_unbiased = (U64.max - bias_rem)
     while true {
-      let u = next()
+      let u = raw()
       if u <= max_unbiased {
-        return u % max
+        return u % end
       }
     }
   }
 
-  func next(min min: U64, max: U64) -> U64 {
+  func u64(min min: U64, end: U64) -> U64 {
+    if min >= end { fatalError("Random.nextU64: min \(min) >= end \(end)") }
+    return u64(end - min) + min
+  }
+
+  func nextInt(end: Int) -> Int {
+    // unbiased random.
+    return Int(u64(U64(end)))
+  }
+
+  func int(min min: Int, end: Int) -> Int {
+    if min >= end { fatalError("Random.nextInt: min \(min) >= end \(end)") }
+    let minU = U64(bitPattern: Int64(min))
+    let endU = U64(bitPattern: Int64(end))
+    let rangeU = endU &- minU
+    let randU = u64(rangeU)
+    return Int(I64(bitPattern: randU + minU))
+  }
+  
+  func f64(max: F64) -> F64 {
+    let endU: U64 = 1 << 52 // double precision has 53 digits; back off by one just to be safe.
+    let u = raw() % endU // powers of two cannot be biased.
+    return (F64(u) / F64(endU - 1)) * max // divide by maxU = endU - 1 to get float in range [0, 1].
+  }
+
+  func f64(min min: F64, max: F64) -> F64 {
     if max <= min {
       return min
     }
-    return next(max - min) + min
-  }
-
-  func next(max: F64) -> F64 {
-    let maxU: U64 = 1 << 52 // double precision has 53 digits; back off by one just to be safe.
-    let u = next() % maxU // powers of two cannot be biased.
-    return F64(u) / F64(maxU)
-  }
-
-  func next(min min: F64, max: F64) -> F64 {
-    if max <= min {
-      return min
-    }
-    return next(max - min) + min
+    return f64(max - min) + min
   }
 }

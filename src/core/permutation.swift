005761dfc14254fb830d730f402e44b9bd0b50b7
George King <george.w.king@gmail.com>
2015-12-02 15:29:48 -0500

    bitRevPermutation.

---
+++ src/core/permutation.swift
@@ -0,0 +1,13 @@
+// © __YEAR__ George King. Permission to use this file is granted in license.txt.
+
+import Foundation
+
+
+func bitRevPermutation(powerOfTwo: Int) -> [Int] {
+  var p = [0]
+  for _ in 0..<powerOfTwo {
+    let p2 = p.map { $0 * 2 }
+    p = p2 + p2.map { $0 + 1 }
+  }
+  return p
+}

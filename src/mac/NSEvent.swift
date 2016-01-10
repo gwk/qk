2c090a8a075ac89c10bfc63bf9b03184547b821e
George King <george.w.king@gmail.com>
2016-01-10 11:47:48 -0500

    NSEvent.unicodeScalar.

---
+++ src/mac/NSEvent.swift
@@ -0,0 +1,19 @@
+// © 2016 George King. Permission to use this file is granted in license-qk.txt.
+
+import Cocoa
+
+
+let unicodeArrowUp    = UnicodeScalar(0xF700)
+let unicodeArrowDown  = UnicodeScalar(0xF701)
+let unicodeArrowLeft  = UnicodeScalar(0xF702)
+let unicodeArrowRight = UnicodeScalar(0xF703)
+
+extension NSEvent {
+
+  var unicodeScalar: UnicodeScalar {
+    let scalars = charactersIgnoringModifiers!.unicodeScalars
+    assert(scalars.count == 1)
+    return scalars.first!
+  }
+
+}

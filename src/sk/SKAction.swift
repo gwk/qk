0987247b93900c00ba3c0ccb302958906cb65a35
George King <george.w.king@gmail.com>
2016-01-26 11:20:00 -0500

    runNodeBlock is now generic with dynamic type check.

--- src/sk/SKAction.swift
+++ src/sk/SKAction.swift
@@ -9,14 +9,19 @@ extension SKAction {
     return SKAction.moveBy(delta * speed, duration: 1).forever
   }
 
-  class func runNodeBlock(block: (SKNode)->()) -> SKAction {
-    return SKAction.customActionWithDuration(0) { // TODO: verify that passing 0 causes this to fire exactly once.
+  class func runNodeBlock<Node: SKNode>(block: (Node)->()) -> SKAction {
+    return SKAction.customActionWithDuration(0) {
       (node: SKNode, elapsedTime: CGFloat) in
-      block(node)
+      if let node = node as? Node {
+        block(node)
+      } else {
+        errL("runNodeBlock cast `SKNode as? \(Node.self)` failed for node: \(node)")
+      }
     }
   }
 
   func delay(duration: Time) -> SKAction { return SKAction.sequence([SKAction.waitForDuration(duration), self]) }
+
   func repeated(count: Int) -> SKAction { return SKAction.repeatAction(self, count: count) }
 
   var forever: SKAction { return SKAction.repeatActionForever(self) }

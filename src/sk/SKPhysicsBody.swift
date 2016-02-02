5c2d4b404af714f5d6d5aeeea8863ebd523c59b9
George King <george.w.king@gmail.com>
2016-02-02 16:57:52 -0500

    SKPhysicsBody.matching(spriteNode:).

--- src/sk/SKPhysicsBody.swift
+++ src/sk/SKPhysicsBody.swift
@@ -8,4 +8,9 @@ extension SKPhysicsBody {
   convenience init(edgeLoopPoints: [CGPoint]) {
     self.init(edgeLoopFromPath: CGPath.with(loopPoints: edgeLoopPoints))
   }
+
+
+  class func matching(spriteNode spriteNode: SKSpriteNode) -> SKPhysicsBody {
+    return SKPhysicsBody(rectangleOfSize: spriteNode.size, center: V2(spriteNode.size) * (V2(0.5, 0.5) - spriteNode.anchorPoint))
+  }
 }

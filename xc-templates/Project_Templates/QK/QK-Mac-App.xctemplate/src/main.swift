5d9e92e7dec804770d9c5782b0d523c6a98a2be8
George King <george.w.king@gmail.com>
2016-02-03 00:14:10 -0500

    add appRandom to template.

--- xc-templates/Project_Templates/QK/QK-Mac-App.xctemplate/src/main.swift
+++ xc-templates/Project_Templates/QK/QK-Mac-App.xctemplate/src/main.swift
@@ -6,6 +6,7 @@ import AppKit
 print("appLaunchSysTime: \(appLaunchSysTime)") // force evaluation of the lazy global to correctly set the time.
 let app = NSApplication.sharedApplication()
 let appDelegate = AppDelegate() // bound to global because app.delegate is unowned.
+let appRandom = Random()
 app.setActivationPolicy(NSApplicationActivationPolicy.Regular)
 app.delegate = appDelegate
 app.run()

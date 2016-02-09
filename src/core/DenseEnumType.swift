767b6fe4eb51816c48a8fe78350320db2efc48c5
George King <george.w.king@gmail.com>
2016-02-09 17:23:34 -0500

    EnumSetU32.

--- src/core/DenseEnumType.swift
+++ src/core/DenseEnumType.swift
@@ -11,3 +11,20 @@ extension DenseEnumType {
   static var range: Range<Int> { return 0..<count }
   static var allVariants: [Self] { return range.map { Self(rawValue: $0)! } }
 }
+
+
+struct EnumSetU32<Element: DenseEnumType>: ArrayLiteralConvertible {
+  let rawValue: U32
+
+  init() {
+    self.rawValue = 0
+  }
+  
+  init(_ element: Element) {
+    self.rawValue = 1 << U32(element.rawValue)
+  }
+
+  init(arrayLiteral elements: Element...) {
+    self.rawValue = elements.reduce(0) { $0 | 1 << U32($1.rawValue) }
+  }
+}

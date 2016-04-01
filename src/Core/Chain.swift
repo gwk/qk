4719363f2f0b1af3e050d448ebaaf128fb7aee90
George King <george.w.king@gmail.com>
2016-04-01 09:52:03 -0400

    improve Chain functionality.

--- src/Core/Chain.swift
+++ src/Core/Chain.swift
@@ -1,16 +1,61 @@
 // © 2015 George King. Permission to use this file is granted in license-qk.txt.
 
 
-enum Chain<T> {
-  case End
-  indirect case Link(T, Chain)
+enum Chain<Element>: IteratorProtocol, Sequence, ArrayLiteralConvertible {
+
+  case end
+  indirect case link(Element, Chain)
   
-  init<C: Collection where C.Generator.Element == T, C.Index: BidirectionalIndex>(_ collection: C) {
-    var c: Chain<T> = .End
+  init<C: Collection where C.Generator.Element == Element, C.Index: BidirectionalIndex>(_ collection: C) {
+    var c: Chain<Element> = .end
     for e in collection.reverse() {
-      c = Chain.Link(e, c)
+      c = .link(e, c)
     }
     self = c
   }
+
+  init(arrayLiteral elements: Element...) {
+    self.init(elements)
+  }
+
+  func generate() -> Chain {
+    return self
+  }
+
+  mutating func next() -> Element? {
+    switch self {
+    case .end: return nil
+    case .link(let hd, let tl):
+      self = tl
+      return hd
+    }
+  }
+
+  var isEmpty: Bool {
+    switch self {
+    case .end: return true
+    case .link: return false
+    }
+  }
 }
 
+
+func ==<Element where Element: Equatable>(l: Chain<Element>, r: Chain<Element>) -> Bool {
+  var l = l
+  var r = r
+  while true {
+    switch l {
+    case .end: return r.isEmpty
+    case .link(let lh, let lt):
+      switch r {
+      case .end: return false
+      case .link(let rh, let rt):
+        if lh != rh {
+          return false
+        }
+        l = lt
+        r = rt
+      }
+    }
+  }
+}

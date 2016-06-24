72a11791f8b8f9627c07ebdc44240926fba3ab2b
George King <george.w.king@gmail.com>
2016-06-24 13:21:05 -0400

    fail(error:); guarded.

--- src/Core/check.swift
+++ src/Core/check.swift
@@ -3,10 +3,7 @@
 
 func check(_ condition: Bool, _ message: @autoclosure () -> String) {
   if !condition {
-    std_err.write("error: ")
-    std_err.write(message())
-    std_err.write("\n")
-    Process.exit(1)
+    fail(message())
   }
 }
 
@@ -21,4 +18,22 @@ func check(_ condition: Bool, file: StaticString = #file, line: UInt = #line) {
   std_err.write(message)
   std_err.write("\n")
   Process.exit(1)
+}
+
+@noreturn func fail(error: ErrorProtocol) {
+  std_err.write("error: ")
+  std_err.write(String(error))
+  std_err.write("\n")
+  Process.exit(1)
+}
+
+func guarded<R>(label: String = "error: ", _ fn: @noescape () throws -> R) -> R {
+  do {
+    return try fn()
+  } catch let e {
+    std_err.write(label)
+    std_err.write(String(e))
+    std_err.write("\n")
+    Process.exit(1)
+  }
 }
